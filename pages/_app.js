c3290606876a176f95ac4f04061e5ccf9e8cde57
GregoireMeillet <meillet.gregoire@gmail.com>
2024-05-23 16:33:44 +0200

    persistor

--- pages/_app.js
+++ pages/_app.js
@@ -2,18 +2,31 @@ import Footer from "@/components/Footer";
 import "@/styles/globals.css";
 
 import { Provider } from "react-redux";
-import { configureStore } from "@reduxjs/toolkit";
 import users from "@/reducers/users";
+import { persistStore, persistReducer } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
+import storage from "redux-persist/lib/storage";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+
+const reducers = combineReducers({ users });
+
+const persistConfig = { key: "IZTA", storage };
 
 const store = configureStore({
-  reducer: { users },
+  reducer: persistReducer(persistConfig, reducers),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false }),
 });
+
+const persistor = persistStore(store);
 
 export default function App({ Component, pageProps }) {
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
-      <Footer />
+      <PersistGate persistor={persistor}>
+        <Component {...pageProps} />
+        <Footer />
+      </PersistGate>
     </Provider>
   );
 }
