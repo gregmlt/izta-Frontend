887055dedbfca90b1fdb8b4b41b3738feda2d888
GregoireMeillet <meillet.gregoire@gmail.com>
2024-05-23 16:58:50 +0200

    ajout linkedin

--- components/UserDataModal.js
+++ components/UserDataModal.js
@@ -32,6 +32,7 @@ export default function UserDataModal({}) {
     ecoute: false,
     recherche: false,
   });
+  const [linkedin, setLinkedin] = useState("");
 
   const token = useSelector((state) => state.users.value.token);
 
@@ -50,6 +51,7 @@ export default function UserDataModal({}) {
         setZipcode(data.data.postalCode);
         setFilters(data.data.diplome);
         setCheckboxes(data.data.situation);
+        setLinkedin(data.data.linkedin);
       });
   }, [token]);
 
@@ -68,6 +70,7 @@ export default function UserDataModal({}) {
       postalCode: zipcode,
       diplome: filters,
       situation: checkboxes,
+      linkedin: linkedin,
     };
 
     fetch(`http://localhost:3000/users/infos/${token}`, {
