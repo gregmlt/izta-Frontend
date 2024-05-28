19e9e2f00b426987dbf7e21876c8a572211142a4
kameamea133 <monnier1977@gmail.com>
2024-05-28 11:50:39 +0200

    admin page frontend side added

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,25 +1,75 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+
+const adminEmail = "v@g.com";
+const adminPassword = "ad";
 
 function index() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    // Fetch the list of users from the server
+    fetch('http://localhost:3000/users')
+    .then(response => response.json())
+    .then((data) => {
+
+        // Update the users state with users that have a non-empty company array
+        setUsers(data.users.filter(user => user.company.length > 0))
+    })
+    .catch(error => console.error('Error fetching users:', error));
+  }, [])
 
   const handleSubmit = (e) => {
-    e.preventDefault();
-    const adminEmail = process.env.REACT_APP_ADMIN_EMAIL;
-    const adminPassword = process.env.REACT_APP_ADMIN_PASSWORD;
+    e.preventDefault(); // Prevent the default form submission behavior
 
+
+     // Check if the entered email and password match the predefined admin credentials
     if (email === adminEmail && password === adminPassword) {
-      setIsLoggedIn(true);
+      setIsLoggedIn(true); // Set the login state to true if credentials are correct
     } else {
       alert('Email ou mot de passe incorrect.');
-      setEmail("");
-      setPassword("");
+      setEmail(""); // Clear the email input
+      setPassword(""); // Clear the password input
     }
   };
 
-  console.log(isLoggedIn)
+
+  
+
+  const handleVerificationChange = (userId, currentVerificationStatus) => {
+
+     // Send a PUT request to the server to update the user's verification status
+    fetch(`http://localhost:3000/users/verification/${userId}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+
+      // Toggle the verification status
+      body: JSON.stringify({ verification: !currentVerificationStatus })
+    })
+      .then(response => response.json())
+      .then((data) => {
+        // Update the users state with the new verification status
+        setUsers(users.map(user => user._id === userId ? { ...user, verification: !currentVerificationStatus, company: !currentVerificationStatus ? [] : user.company } : user));
+      })
+      
+      .catch(error => {
+        console.error('Error updating verification status:', error);
+      });
+  };
+
+  
+  // logout function
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+    setEmail('');
+    setPassword('');
+  };
+
+  
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
     {!isLoggedIn ? (
@@ -55,9 +105,39 @@ function index() {
         </form>
       </div>
     ) : (
-      <div className="bg-white p-8 rounded shadow-md w-80">
-        <h2 className="text-2xl font-bold mb-6 text-center">Bienvenue Melanie</h2>
-      </div>
+        <div className="bg-white p-8 rounded-lg shadow-xl w-[500px]">
+  <h2 className="text-3xl font-bold mb-6 text-center text-indigo-600">Bienvenue Melanie</h2>
+  <p className="py-5 underline font-semibold text-center text-gray-700">Personnes en attente de vérification</p>
+  <ul className="space-y-4">
+    {users.map(user => (
+      <li key={user._id} className="flex flex-col sm:flex-row items-start sm:items-center justify-between p-4 border rounded-lg bg-gray-50 hover:bg-gray-100 transition">
+        <div className="flex-1">
+          <p className="block text-lg text-gray-800"><span className='font-bold mb-2'>Nom:</span> {user.firstname} {user.lastname}</p>
+          <p className="text-gray-600 mb-2"><span className='font-bold'>Email:</span> {user.email}</p>
+          <p className="text-gray-600 mb-2"><span className='font-bold'>Nom de l'entreprise:</span> {user.company[0].companyName}</p>
+        </div>
+        <div className="mt-4 sm:mt-0 sm:ml-4">
+          <label className="inline-flex items-center">
+            <input
+              type="checkbox"
+              className="form-checkbox h-5 w-5 text-indigo-600"
+              checked={user.verification}
+              onChange={() => handleVerificationChange(user._id, user.verification)}
+            />
+            <span className="ml-2 text-gray-700">Vérifié</span>
+          </label>
+        </div>
+      </li>
+    ))}
+  </ul>
+  <button
+    onClick={handleLogout}
+    className="w-full bg-red-600 text-white py-2 px-4 rounded-lg mt-6 hover:bg-red-700 transition"
+  >
+    Déconnexion
+  </button>
+</div>
+
     )}
   </div>
   )
