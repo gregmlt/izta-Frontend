4d2cbe74e86fac61a4bbbd15d21532f6d6ff7d94
NaeemDy <naim.hamidi@gmail.com>
2024-05-29 14:49:52 +0200

    companymodal

--- components/CompanySearchResultsModal.js
+++ components/CompanySearchResultsModal.js
@@ -10,6 +10,11 @@ function CompanySearchResultsModal({ name, taille, starsCount, imageSrc }) {
     return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
   };
 
+  const router = useRouter();
+  const handleCompanyPage = () => {
+    router.push(`/company/${companyId}`);
+  };
+
   const renderStars = (count) => {
     const stars = [];
     for (let i = 0; i < 5; i++) {
@@ -38,18 +43,21 @@ function CompanySearchResultsModal({ name, taille, starsCount, imageSrc }) {
     if (text.length <= maxLength) {
       return text;
     }
-    return text.slice(0, maxLength) + '...';
+    return text.slice(0, maxLength) + "...";
   };
 
   return (
-    <div className="p-6 bg-white mt-10 h-auto rounded-md flex flex-col items-start w-[95%] m-4 border hover:shadow-lg hover:border-[#003761] cursor-pointer transition ease-in-out 000ms hover-slide-up">
-     <div className="w-full flex h-[190px] object-cover rounded-md bg-[linear-gradient(to_right_top,rgba(206,100,38,0.2),rgba(16,34,93,0.8)),url('/images/campain-asso.jpg')] bg-cover">
-     <div className="flex items-start w-full p-3">
+    <div
+      className="p-6 bg-white mt-10 h-auto rounded-md flex flex-col items-start w-[95%] m-4 border hover:shadow-lg hover:border-[#003761] cursor-pointer transition ease-in-out 000ms hover-slide-up"
+      onClick={handleCompanyPage}
+    >
+      <div className="w-full flex h-[190px] object-cover rounded-md bg-[linear-gradient(to_right_top,rgba(206,100,38,0.2),rgba(16,34,93,0.8)),url('/images/campain-asso.jpg')] bg-cover">
+        <div className="flex items-start w-full p-3">
           <div className="ml-auto z-10">
             <HeartSVGIcons stroke="stroke-white" />
           </div>
         </div>
-     </div>
+      </div>
       {/* <img
         src="https://images.unsplash.com/photo-1512403754473-27835f7b9984?q=80&w=1915&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         alt={name}
@@ -57,13 +65,11 @@ function CompanySearchResultsModal({ name, taille, starsCount, imageSrc }) {
       /> */}
       <div className="flex flex-col w-full mt-4">
         <div className="flex items-center justify-between">
-        <h2 className="text-2xl font-semibold text-[#003761] mb-3">
+          <h2 className="text-2xl font-semibold text-[#003761] mb-3">
             {truncateText(capitalizeFirstLetter(name), 25)}
           </h2>
-
         </div>
-        <div className="flex">{renderStars(starsCount)}
-        </div>
+        <div className="flex">{renderStars(starsCount)}</div>
         <p className="text-sm mt-5">
           <span className="font-semibold">Secteur :</span> Industrie énergétique
           et de la construction
